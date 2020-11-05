cd6462ebb3430711e3bd1783f74115b0f1e8e059
Johann Folta <jfolta@kyruus.com>
2020-11-05 12:15:40 -0500

    exercise 1

--- src/00/Timer.js
+++ src/00/Timer.js
@@ -1,75 +1,72 @@
-// import * as React from 'react';
-// import { useReducer } from 'react';
-// import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { ProgressCircle } from '../ProgressCircle';
+import * as React from "react";
+import { useReducer } from "react";
+import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ProgressCircle } from "../ProgressCircle";
 
-// // Import the timer machine and its initial state:
-// // import { ... } from './timerMachine';
+import { timerMachine, timerMachineConfig } from "./timerMachine";
 
-// export const Timer = () => {
-//   const state = ''; // delete me - useReducer instead!
+export const Timer = () => {
+  const [state, dispatch] = useReducer(
+    timerMachine,
+    timerMachineConfig.initial
+  );
 
-//   const { duration, elapsed, interval } = {
-//     duration: 60,
-//     elapsed: 0,
-//     interval: 0.1,
-//   };
+  const { duration, elapsed, interval } = {
+    duration: 60,
+    elapsed: 0,
+    interval: 0.1,
+  };
 
-//   return (
-//     <div
-//       className="timer"
-//       data-state={state}
-//       style={{
-//         // @ts-ignore
-//         '--duration': duration,
-//         '--elapsed': elapsed,
-//         '--interval': interval,
-//       }}
-//     >
-//       <header>
-//         <h1>Exercise 00</h1>
-//       </header>
-//       <ProgressCircle />
-//       <div className="display">
-//         <div className="label">{state}</div>
-//         <div
-//           className="elapsed"
-//           onClick={() => {
-//             // ...
-//           }}
-//         >
-//           {Math.ceil(duration - elapsed)}
-//         </div>
-//         <div className="controls">
-//           <button
-//             onClick={() => {
-//               // ...
-//             }}
-//           >
-//             Reset
-//           </button>
-//         </div>
-//       </div>
-//       <div className="actions">
-//         <button
-//           onClick={() => {
-//             // ...
-//           }}
-//           title="Pause timer"
-//         >
-//           <FontAwesomeIcon icon={faPause} />
-//         </button>
-
-//         <button
-//           onClick={() => {
-//             // ...
-//           }}
-//           title="Start timer"
-//         >
-//           <FontAwesomeIcon icon={faPlay} />
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div
+      className="timer"
+      data-state={state}
+      style={{
+        // @ts-ignore
+        "--duration": duration,
+        "--elapsed": elapsed,
+        "--interval": interval,
+      }}
+    >
+      <header>
+        <h1>Exercise 00</h1>
+      </header>
+      <ProgressCircle />
+      <div className="display">
+        <div className="label">{state}</div>
+        <div
+          className="elapsed"
+          onClick={() => {
+            dispatch({ type: "TOGGLE" });
+          }}
+        >
+          {Math.ceil(duration - elapsed)}
+        </div>
+        <div className="controls">
+          {state === "paused" && (
+            <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
+          )}
+        </div>
+      </div>
+      <div className="actions">
+        {state === "running" && (
+          <button
+            onClick={() => dispatch({ type: "TOGGLE" })}
+            title="Pause timer"
+          >
+            <FontAwesomeIcon icon={faPause} />
+          </button>
+        )}
+        {(state === "paused" || state === "idle") && (
+          <button
+            onClick={() => dispatch({ type: "TOGGLE" })}
+            title="Start timer"
+          >
+            <FontAwesomeIcon icon={faPlay} />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
