9a9a9235bc974450dcdc1f57af88d67fdbd1b0fe
Johann Folta <jfolta@kyruus.com>
2020-11-05 16:24:47 -0500

    exercise 6

--- src/06/Timer.js
+++ src/06/Timer.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import { useEffect } from 'react';
-import { faPlay, faPause, faStop } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as React from "react";
+import { useEffect } from "react";
+import { faPlay, faPause, faStop } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { useMachine } from '@xstate/react';
-import { ProgressCircle } from '../ProgressCircle';
+import { useMachine } from "@xstate/react";
+import { ProgressCircle } from "../ProgressCircle";
 
-import { timerMachine } from './timerMachine';
+import { timerMachine } from "./timerMachine";
 
 export const Timer = () => {
   const [state, send] = useMachine(timerMachine);
@@ -15,7 +15,7 @@ export const Timer = () => {
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      send('TICK');
+      send("TICK");
     }, interval * 1000);
 
     return () => clearInterval(intervalId);
@@ -24,12 +24,12 @@ export const Timer = () => {
   return (
     <div
       className="timer"
-      data-state={state.toStrings().join(' ')}
+      data-state={state.toStrings().join(" ")}
       style={{
         // @ts-ignore
-        '--duration': duration,
-        '--elapsed': elapsed,
-        '--interval': interval,
+        "--duration": duration,
+        "--elapsed": elapsed,
+        "--interval": interval,
       }}
     >
       <header>
@@ -38,32 +38,33 @@ export const Timer = () => {
       <ProgressCircle />
       <div className="display">
         <div className="label">{state.toStrings().slice(-1)}</div>
-        <div className="elapsed" onClick={() => send('TOGGLE')}>
+        <div className="elapsed" onClick={() => send("TOGGLE")}>
           {Math.ceil(duration - elapsed)}
         </div>
         <div className="controls">
-          {!state.matches({ running: 'normal' }) && (
-            <button onClick={() => send('RESET')}>Reset</button>
+          {!state.matches({ running: "normal" }) && (
+            <button onClick={() => send("RESET")}>Reset</button>
           )}
 
-          {state.matches({ running: 'normal' }) && (
-            <button onClick={() => send('ADD_MINUTE')}>+ 1:00</button>
+          {state.matches({ running: "normal" }) && (
+            <button onClick={() => send("ADD_MINUTE")}>+ 1:00</button>
           )}
         </div>
       </div>
       <div className="actions">
-        {state.matches({ running: 'normal' }) && (
-          <button onClick={() => send('TOGGLE')} title="Pause timer">
+        {state.matches({ running: "normal" }) && (
+          <button onClick={() => send("TOGGLE")} title="Pause timer">
             <FontAwesomeIcon icon={faPause} />
           </button>
         )}
-        {state.matches({ running: 'overtime' }) && (
-          <button onClick={() => send('RESET')} title="Reset timer">
+        {/* when overtime, button becomes reset */}
+        {state.matches({ running: "overtime" }) && (
+          <button onClick={() => send("RESET")} title="Reset timer">
             <FontAwesomeIcon icon={faStop} />
           </button>
         )}
-        {(state.matches('paused') || state.matches('idle')) && (
-          <button onClick={() => send('TOGGLE')} title="Start timer">
+        {(state.matches("paused") || state.matches("idle")) && (
+          <button onClick={() => send("TOGGLE")} title="Start timer">
             <FontAwesomeIcon icon={faPlay} />
           </button>
         )}
