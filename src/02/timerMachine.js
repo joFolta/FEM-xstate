c629de49a288d499fa0454325306f86bac56f739
Johann Folta <jfolta@kyruus.com>
2020-11-05 13:04:11 -0500

    exercise 2

--- src/02/timerMachine.js
+++ src/02/timerMachine.js
@@ -1,31 +1,36 @@
-import { createMachine, assign } from 'xstate';
+import { createMachine, assign } from "xstate";
 
 export const timerMachine = createMachine({
-  initial: 'idle',
-  // Add initial context
-  // ...
-
+  initial: "idle",
+  context: {
+    duration: 60,
+    elapsed: 0,
+    interval: 0.1,
+  },
   states: {
     idle: {
-      // Reset duration and elapsed on entry
-      // ...
-
+      entry: assign({
+        duration: 60,
+        elapsed: 0,
+      }),
       on: {
-        TOGGLE: 'running',
+        TOGGLE: "running",
       },
     },
     running: {
       on: {
-        TOGGLE: 'paused',
-
-        // On ADD_MINUTE, increment context.duration by 60 seconds
-        // ...
+        TOGGLE: "paused",
+        ADD_MINUTE: {
+          actions: assign({
+            duration: (ctx) => ctx.duration + 60,
+          }),
+        },
       },
     },
     paused: {
       on: {
-        TOGGLE: 'running',
-        RESET: 'idle',
+        TOGGLE: "running",
+        RESET: "idle",
       },
     },
   },
